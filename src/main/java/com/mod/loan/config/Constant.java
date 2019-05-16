09b1bb0825b9872c262b2619aafb5b791ad6abc5
LJJ <liujianjian@booksroo.com>
2019-05-16 19:41:59 +0800

    日志配置

--- src/main/java/com/mod/loan/config/Constant.java
+++ src/main/java/com/mod/loan/config/Constant.java
@@ -24,6 +24,38 @@ public class Constant {
 
     public static String kuaiQianKeyPassword;
 
+    public static String rongZeRequestAppId;
+    public static String rongZeCallbackUrl;
+    public static String rongZeQueryUrl;
+    public static String rongZePublicKey;
+
+    public static String orgPrivateKey;
+
+    @Value("${rongze.request.app.id}")
+    public void setRongZeRequestAppId(String rongZeRequestAppId) {
+        Constant.rongZeRequestAppId = rongZeRequestAppId;
+    }
+
+    @Value("${rongze.callback.url}")
+    public void setRongZeCallbackUrl(String rongZeCallbackUrl) {
+        Constant.rongZeCallbackUrl = rongZeCallbackUrl;
+    }
+
+    @Value("${rongze.query.url}")
+    public void setRongZeQueryUrl(String rongZeQueryUrl) {
+        Constant.rongZeQueryUrl = rongZeQueryUrl;
+    }
+
+    @Value("${org.rsa.private.key}")
+    public void setOrgPrivateKey(String orgPrivateKey) {
+        Constant.orgPrivateKey = orgPrivateKey;
+    }
+
+    @Value("${rongze.rsa.public.key}")
+    public void setRongZePublicKey(String rongZePublicKey) {
+        Constant.rongZePublicKey = rongZePublicKey;
+    }
+
     @Value("${kuaiqian.key.password}")
     public void setKuaiQianKeyPassword(String kuaiQianKeyPassword) {
         Constant.kuaiQianKeyPassword = kuaiQianKeyPassword;
