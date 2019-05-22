71a52191418d0de50d550dffd58b2d8e8b1d4d3e
shuailj <178397218@qq.com>
2019-05-22 15:28:25 +0800

    时间戳修改

--- src/main/java/com/mod/loan/service/impl/CallBackRongZeServiceImpl.java
+++ src/main/java/com/mod/loan/service/impl/CallBackRongZeServiceImpl.java
@@ -102,9 +102,9 @@ public class CallBackRongZeServiceImpl implements CallBackRongZeService {
             repay.put("bill_status", "1");
         }
         // 账单到期时间 精确到毫秒（比如 1539073086805)
-        repay.put("due_time", new DateTime(order.getRepayTime()).toDateTime().getMillis() / 1000);
+        repay.put("due_time", new DateTime(order.getRepayTime()).toDateTime().getMillis());
         // 当期最早可以还款的时间 精确到毫秒（比如 1539073086805 ）
-        repay.put("can_repay_time", System.currentTimeMillis() / 1000);
+        repay.put("can_repay_time", System.currentTimeMillis());
         // 还款方式：1=主动还款 2=跳转机构 H5 还款  4=银行代扣 5=主动还款+银行代扣
         repay.put("pay_type", 5);
         // 当前所需的还款金额，单位元，保留小数点后两位 （该金额应该是本金利息加上逾期金额减去已还款金额的结果，逾期金额、已还款金额可能为零）
