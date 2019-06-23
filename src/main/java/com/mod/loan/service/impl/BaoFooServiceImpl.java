660041eb48fdb6b54119672b57e277f2a82c3976
shuailj <178397218@qq.com>
2019-06-23 16:45:15 +0800

    代码修改

--- src/main/java/com/mod/loan/service/impl/BaoFooServiceImpl.java
+++ src/main/java/com/mod/loan/service/impl/BaoFooServiceImpl.java
@@ -57,7 +57,7 @@ public class BaoFooServiceImpl implements BaoFooService {
                 userBank.setForeignId(protocolNo);
                 userBank.setUpdateTime(new Date());
                 userBankService.updateByPrimaryKey(userBank);
-                log.info("BaoFooServiceImpl.bindQuery, update protocolNo success, userId={}");
+                log.info("BaoFooServiceImpl.bindQuery, update protocolNo success, userId={}",userBank.getUid());
             }
         }
     }
@@ -111,7 +111,7 @@ public class BaoFooServiceImpl implements BaoFooService {
 
         Map<String, String> returnData = FormatUtil.getParm(postString);
 
-        System.out.println(JSON.toJSONString(returnData));
+        log.info("绑卡查询返回参数:{}", JSON.toJSONString(returnData));
 
         if (!returnData.containsKey("signature")) {
             throw new Exception("缺少验签参数！");
